488555ff7bec74650ed6cc885d03483c52c3e05a
Jason Ng <>
2023-01-14 11:11:27 +0800

    added header and bootstrap components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#home">Welcome</Navbar.Brand>
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav> */}
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
--- server/server.py
+++ server/server.py
@@ -3,11 +3,11 @@ from flask import Flask
 # Initializing flask app
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home_page():
   
     # Returning an api for showing in  reactjs
-    return 'ok'
+    return {"response": "ok"}, 200
   
       
 # Running app
