41e8616065b0133dc250cbb3c78b34b22a633ee5
Jason Ng <>
2023-01-16 23:53:58 +0800

    added about component

---
+++ client/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="about-intro">
+        <p>Hello</p>
+      </div>
+      <div className="about-bg">
+        <img src="/images/cartoon-food.png" alt="cartoon-food-pic"/>
+      </div>
+      <div className="about-end"></div>
+    </div>
+  );
+}
+
+export default About;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import About from "./About";
 
 function App() {
     return (
         <div>
             <Header/>
+            <About />
             <Footer/>
         </div>
     );
