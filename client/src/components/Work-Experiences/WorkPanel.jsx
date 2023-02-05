739ae005051d91175d3ae16821e033a02b558446
Jason Ng <>
2023-02-05 21:06:35 +0800

    edited styling for workpanel

--- client/src/components/Work-Experiences/WorkPanel.jsx
+++ client/src/components/Work-Experiences/WorkPanel.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import styles from './styles.css'
+import Image from 'react-bootstrap/Image'
 
 function WorkPanel(props) {
 	return (
 		<Row className={styles.companyCon} >
-			<Col xs={2}>
-				<img src={`/images/work-exp/${props.image}.png`} alt="work-experience.png" className={styles.companyPic} />
+			<Col className={styles.companyPicCon} xs={2}>
+				<Image fluid roundedCircle src={`/images/work-exp/${props.image}.png`} alt="work-experience.png" className={styles.companyPic} />
 			</Col>
 			<Col xs={4}>
-				<p>{props.title}</p>
-				<p>{props.company}</p>
-				<p>{props.duration}</p>
+				<p className={styles.companyIntro}>{props.title}</p>
+				<p className={styles.companyIntro}>{props.company}</p>
+				<p className={styles.companyIntro}>{props.duration}</p>
 			</Col>
 			<Col xs={6}>
-				<ul>
+				<ul className={styles.companyUl}>
 					{props.list.map((item, i) => <li key={i} className={styles.companyDetails}>{item}</li>)}
 				</ul>
 			</Col>
